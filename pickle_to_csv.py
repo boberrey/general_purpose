1d87433409d0d49a7bce6dbd4c20fbea5863e4ba
boberry <boberry@GEN-D25R40A4GG7V.stanford.edu>
2016-10-18 10:06:42 -0700

    wrote the pickle_to_csv.py script

---
+++ pickle_to_csv.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+""" 
+Read in a pickled file and copy it as a csv
+
+Inputs:
+	pickled file
+
+Outputs:
+    csv
+
+Ben Ober-Reynolds, boberrey@stanford.edu
+20161018
+"""
+
+import pandas as pd
+import argparse
+import sys
+
+def main():
+	################ Parse input parameters ################
+
+	#set up command line argument parser
+	parser = argparse.ArgumentParser(description='Script for converting a pickle file to csv')
+	group = parser.add_argument_group('required arguments')
+	group.add_argument('-p', '--pickle_file', required=True,
+	                    help='The pickle file that you want to read')
+	group = parser.add_argument_group('optional arguments')
+	group.add_argument('-od','--output_directory', default="",
+	                    help='output directory for the new csv. Default is same directory as pickle file.')
+	if not len(sys.argv) > 1:
+		parser.print_help()
+		sys.exit()
+	#parse command line arguments
+	args = parser.parse_args()
+
+	# Set path
+	output_dir = ""
+	if args.output_directory == "":
+		output_dir = os.path.dirname(os.path.abspath(args.pickle_file))
+	else:
+		output_dir = args.output_directory
+
+	# Prepare new file name
+	filename = os.path.splitext(os.path.basename(args.pickle_file))[0] + ".csv"
+
+	# Read data in
+	df = pd.read_pickle(args.pickle_file)
+
+	# Print data out
+	df.to_csv(output_dir + filename)
+
+
+
+if __name__ == '__main__':
+    main()
